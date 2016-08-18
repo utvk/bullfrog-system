eeb774025d448645628268c250f93c28caae55d0
utvk <utvk@users.noreply.github.com>
2016-08-18 23:56:18 +0200

    tweak

--- bin/recompile-steem.bash
+++ bin/recompile-steem.bash
@@ -16,7 +16,7 @@ git clone https://github.com/steemit/steem.git
 cd steem
 
 git submodule update --init --recursive
-./steem/programs/build_helpers/configure_build.py
+./programs/build_helpers/configure_build.py
 #cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_CONTENT_PATCHING=OFF -DLOW_MEMORY_NODE=ON .
 make -j$(grep -c ^processor /proc/cpuinfo)
 sudo make install
